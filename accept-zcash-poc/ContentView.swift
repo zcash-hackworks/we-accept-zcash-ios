2279a704a98fae42b20a6bfca92044853a4998e2
Francisco Gindre <francisco.gindre@gmail.com>
2021-02-19 10:11:22 -0300

    Initial Commit

---
+++ accept-zcash-poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  accept-zcash-poc
+//
+//  Created by Francisco Gindre on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ accept-zcash-poc/accept_zcash_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  accept_zcash_pocApp.swift
+//  accept-zcash-poc
+//
+//  Created by Francisco Gindre on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct accept_zcash_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
